1aa1ba22e9e752d7f938ecc9bef2490cb6c2ab7a
Grace <gracebolutife@gmail.com>
2021-01-09 01:04:43 +0100

    test

--- test/codeExercises/AverageTest.java
+++ test/codeExercises/AverageTest.java
@@ -2,15 +2,19 @@ package codeExercises;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AverageTest {
     Average average;
+
+    Random random = new Random();
 
     @BeforeEach
     void setUp() {
@@ -29,6 +33,16 @@ class AverageTest {
         numbers.add(50);
 
         assertEquals(41.0, average.findAverage(numbers));
+    }
 
+    @Test
+    @DisplayName("Test that code is efficient when i try to find average of long list of numbers")
+    void testThatICanFindAverageOfALongListOfNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+        for(int size = 0; size <=100; size++){
+            numbers.add(random.nextInt(500));
+        }
+
+        System.out.println(average.findAverage(numbers));
     }
 }
