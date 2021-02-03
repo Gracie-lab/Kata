76b043b0a4db08f765e9063f200c92ed50fc56f2
Grace <gracebolutife@gmail.com>
2021-02-03 13:19:00 +0100

    .

--- src/Hackerrank/HourGlass.java
+++ src/Hackerrank/HourGlass.java
@@ -2,7 +2,7 @@ package Hackerrank;
 
 //this program gets the highest sum of hourglass in a 6by6 array
 public class HourGlass {
-    int highestSumOfHourGlass = 0;
+    int highestSumOfHourGlass = Integer.MIN_VALUE;
     private static int[][] array = new int[6][6];
 
     public int getHighestSumOfHourglass(){
@@ -12,9 +12,7 @@ public class HourGlass {
                         + array[row + 1][column + 1] + array[row + 2][column] + array[row + 2][column + 1]
                         + array[row + 2][column + 2];
 
-                if (sumOfHourGlass > highestSumOfHourGlass) {
-                    highestSumOfHourGlass = sumOfHourGlass;
-                }
+                highestSumOfHourGlass = Math.max(highestSumOfHourGlass, sumOfHourGlass);
             }
 
         }
