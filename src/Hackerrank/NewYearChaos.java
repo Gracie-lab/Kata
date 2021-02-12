513a46537bd5bdbb49d6f6eeb22764bf16b73171
Grace <gracebolutife@gmail.com>
2021-02-12 04:54:47 +0100

    ..

--- src/Hackerrank/NewYearChaos.java
+++ src/Hackerrank/NewYearChaos.java
@@ -3,19 +3,15 @@ package Hackerrank;
 public class NewYearChaos {
 
     public void getNumberOfCheats(int[] array){
-        String chaotic = " ";
-        int numberOfCheats = 0;
             for (int index = 0; index < array.length; index++) {
-                if (array[index] - index + 1 > 2) {
-                    System.out.println(array[index]-(index+1));
+
+                int differenceInLocation = array[index]-(index+1);
+
+                System.out.println(array[differenceInLocation);
+                if (differenceInLocation > 2) {
                     System.out.println("Too chaotic");
                     return;
                 }
-                else {
-                    System.out.println(array[index]-(index+1));
-                }
             }
-
-
     }
 }
