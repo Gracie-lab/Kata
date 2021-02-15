e09af0b5b4ed55080b55191b6db4ac3569e51980
Grace <gracebolutife@gmail.com>
2021-02-15 22:30:11 +0100

    new year chaos hackerrank solution

--- src/Hackerrank/NewYearChaos.java
+++ src/Hackerrank/NewYearChaos.java
@@ -1,17 +1,40 @@
 package Hackerrank;
 
+//It is New Year's Day and people are in line for the Wonderland rollercoaster ride.
+// Each person wears a sticker indicating their initial position in the queue from  to .
+// Any person can bribe the person directly in front of them to swap positions,
+// but they still wear their original sticker. One person can bribe at most two others.
+//Determine the minimum number of bribes that took place to get to a given queue order.
+// Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+
 public class NewYearChaos {
 
     public void getNumberOfCheats(int[] array){
-            for (int index = 0; index < array.length; index++) {
 
-                int differenceInLocation = array[index]-(index+1);
+        int totalBribes = 0;
 
-                System.out.println(array[differenceInLocation);
-                if (differenceInLocation > 2) {
-                    System.out.println("Too chaotic");
-                    return;
-                }
+        int expectedFirst = 1;
+        int expectedSecond = 2;
+        int expectedThird = 3;
+
+        for (int number : array) {
+            if (number == expectedFirst) {
+                expectedFirst = expectedSecond;
+                expectedSecond = expectedThird;
+                ++expectedThird;
+            } else if (number == expectedSecond) {
+                ++totalBribes;
+                expectedSecond = expectedThird;
+                ++expectedThird;
+            } else if (number == expectedThird) {
+                totalBribes += 2;
+                ++expectedThird;
+            } else {
+                System.out.println("Too chaotic");
+                return;
             }
+        }
+
+        System.out.println(totalBribes);
     }
 }
